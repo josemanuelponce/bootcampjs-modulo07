a199f557d9764b24ea89bf9e65f79c03d82f0e42
Nombre prueba <prueba@hotmail.com>
2024-01-24 13:43:45 +0100

    devolverMensajePlantarse terminado

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -1,4 +1,6 @@
-import {generarCarta} from './motor';
+import { vi } from 'vitest';
+import { generarCarta, devolverMensajePlantarse } from './motor';
+import { puntos } from './model';
 
   describe('generarCarta', () => {
     it("Generar carta deberia devolver 12 cuando el numero es 10", () => {
@@ -44,6 +46,61 @@ import {generarCarta} from './motor';
       // Assert
       expect(carta).toBe(valorEsperado);
     });
+
+  });
+
+
+
+
+  describe('devolverMensajePlantarse', () => {
+    it("Cuando tu puntuacion es 7.5 deberia devolver ¡Lo has clavado! ¡Enhorabuena!", () => {
+      // Arrange
+      const valorEsperado: string = "¡Lo has clavado! ¡Enhorabuena!";
+      vi.spyOn(puntos, "puntuacion", "get").mockReturnValue(7.5);
+
+      // Act
+      const valor = devolverMensajePlantarse();
+
+      // Assert
+      expect(valor).toBe(valorEsperado);
+    });
+
+    it("Cuando tu puntuacion es 14 deberia devolver Game Over", () => {
+      // Arrange
+      const valorEsperado: string = "Game Over";
+      vi.spyOn(puntos, "puntuacion", "get").mockReturnValue(14);
+
+      // Act
+      const valor = devolverMensajePlantarse();
+
+      // Assert
+      expect(valor).toBe(valorEsperado);
+    });
+
+    it("Cuando tu puntuacion es 5.5 deberia devolver Te ha entrado el canguelo eh?", () => {
+      // Arrange
+      const valorEsperado: string = "Te ha entrado el canguelo eh?";
+      vi.spyOn(puntos, "puntuacion", "get").mockReturnValue(5.5);
+
+      // Act
+      const valor = devolverMensajePlantarse();
+
+      // Assert
+      expect(valor).toBe(valorEsperado);
+    });
+
+    it("Cuando tu puntuacion es 7 deberia devolver Casi casi...", () => {
+      // Arrange
+      const valorEsperado: string = "Casi casi...";
+      vi.spyOn(puntos, "puntuacion", "get").mockReturnValue(7);
+
+      // Act
+      const valor = devolverMensajePlantarse();
+
+      // Assert
+      expect(valor).toBe(valorEsperado);
+    });
+
   });
 
   
